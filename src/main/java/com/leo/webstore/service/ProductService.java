f4ed8f3953dbeda044a3019d5fca9fab7c4456ae
Leogaogithub <longhaogao@gmail.com>
2016-10-02 19:18:27 -0500

    adding the product details page

--- src/main/java/com/leo/webstore/service/ProductService.java
+++ src/main/java/com/leo/webstore/service/ProductService.java
@@ -1,9 +1,14 @@
 package com.leo.webstore.service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.leo.webstore.domain.Product;
 
 public interface ProductService {
 	List <Product> getAllProducts();
+	List<Product> getProductsByCategory(String category); 
+	Set<Product> getProductsByFilter(Map<String, List<String>> filterParams);
+	Product getProductById(String productId);
 }
--- src/main/java/com/leo/webstore/service/impl/ProductServiceImpl.java
+++ src/main/java/com/leo/webstore/service/impl/ProductServiceImpl.java
@@ -1,6 +1,8 @@
 package com.leo.webstore.service.impl;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +18,15 @@ public class ProductServiceImpl implements ProductService {
 	public List<Product> getAllProducts() {		
 		return productRepository.getAllProducts();
 	}
-
+	public List<Product> getProductsByCategory(String category) {
+		  return productRepository.getProductsByCategory(category);
+		}
+	
+	public Set<Product> getProductsByFilter(Map<String, List<String>> filterParams) {
+	    return productRepository.getProductsByFilter(filterParams);
+	}
+	
+	public Product getProductById(String productId){
+		return productRepository.getProductById(productId);
+	}
 }
